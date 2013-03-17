031645c1accc622be524f7b605d415b1043cce72
cheresharov@hotmail.com <>
2013-03-17 09:39:34 +0000

    Added the model

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,19 @@
  * file.
  */
 
+<?php
 return array(
-    // ...
+    'db' => array(
+        'driver'         => 'Pdo',
+      // 'dsn'            => 'mysql:dbname=zf2tutorial;host=localhost',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+                    => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
 );
